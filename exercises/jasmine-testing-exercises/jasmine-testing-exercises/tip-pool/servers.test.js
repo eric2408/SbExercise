d5bb74e84c2f85ba6d0a5353c3557c13d4df9c5b
eric2408 <kimeric3797@gmail.com>
2022-05-24 23:17:58 -0700

    test functions

--- exercises/jasmine-testing-exercises/jasmine-testing-exercises/tip-pool/servers.test.js
+++ exercises/jasmine-testing-exercises/jasmine-testing-exercises/tip-pool/servers.test.js
@@ -11,7 +11,29 @@ describe("Servers test (with setup and tear-down)", function() {
     expect(allServers['server' + serverId].serverName).toEqual('Alice');
   });
 
+  it('no servers on the list when submitServerInfo() is empty', function () {
+    serverNameInput.value='';
+    submitServerInfo();
+
+    expect(Object.keys(allServers).length).toEqual(0);
+  });
+
+  it('should update on the table when updateServerTable()', function () {
+    submitServerInfo();
+    updateServerTable();
+
+    let serveList = document.querySelectorAll('#serverTable tbody tr td');
+
+    expect(serveList.length).toEqual(3);
+    expect(serveList[0].innerText).toEqual('Alice');
+    expect(serveList[1].innerText).toEqual('$0.00');
+    expect(serveList[2].innerText).toEqual('X');
+  });
+
   afterEach(function() {
     // teardown logic
+    serverTbody.innerHTML = '';
+    allServers = {};
+    serverId = 0;
   });
 });
