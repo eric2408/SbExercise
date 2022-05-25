774681f4f23e52fa8c80af59c1b26284eed48e86
eric2408 <kimeric3797@gmail.com>
2022-05-25 14:25:47 -0700

    some rev

--- exercises/array-exercise/some.js
+++ exercises/array-exercise/some.js
@@ -37,10 +37,3 @@ function hasCertainValue(arr, key, value){
     })
 }
 
-let arri = [
-    {title: "Instructor", first: 'Elie', last:"Schoppik"},
-    {title: "Instructor", first: 'Tim', last:"Garcia", isCatOwner: true},
-    {title: "Instructor", first: 'Matt', last:"Lane"},
-    {title: "Instructor", first: 'Colt', last:"Steele", isCatOwner: true}
-  ]
-
